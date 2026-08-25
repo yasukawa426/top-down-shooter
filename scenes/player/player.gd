fb63a77cc38b4b4d23f65f3399a10e3dd6fe8b90
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-08-25 13:58:43 -0300

    feat: make player rotate towards mouse

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -8,13 +8,11 @@ const JUMP_VELOCITY: float = -400.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	# if not is_on_floor():
-	# 	velocity += get_gravity() * delta
+	#rotate towards the mouse
+	look_at(get_global_mouse_position())
+	
 
-	# # Handle jump.
-	# if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	# 	velocity.y = JUMP_VELOCITY
+
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
