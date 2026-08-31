a16a692770b14cba4e96af7cfff659441b99b0a3
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-08-31 15:59:33 -0300

    feat: update enemy to spawn with random scale and speed, and add debug speed label

--- scenes/enemies/enemy.gd
+++ scenes/enemies/enemy.gd
@@ -2,10 +2,21 @@ extends CharacterBody2D
 
 
 @export var SPEED = 200
+#enemy max speed, rn its slighty lower than the player
+const _MAX_SPEED = 500
 var player:Node2D
 
 
-
+func _ready() -> void:
+	var rand_scale := randf_range(0.6, 1.4)
+	scale = Vector2(rand_scale, rand_scale)
+	
+	#the smaller the faster, we use power so it is even more noticiable
+	set_speed(SPEED / pow(rand_scale, 1.5))
+	
+	if OS.is_debug_build():
+		$DebugSpeedLabel.visible = true
+		$DebugSpeedLabel.text = str(int(SPEED))
 
 func _physics_process(delta: float) -> void:
 	velocity = Vector2.ZERO
@@ -33,3 +44,11 @@ func _physics_process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	queue_free()
 	body.queue_free()
+
+#updated the enemy speed respecting its max speed
+func set_speed(speed: float) -> void:
+	if speed > _MAX_SPEED:
+		speed = _MAX_SPEED
+		
+	SPEED = speed 
+	
