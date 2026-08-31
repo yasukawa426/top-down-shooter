2ce5e158bbb2b878438449affab96ef36bb96191
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-08-31 15:58:40 -0300

    feat: move level test to its own scene and make it spawn enemies along the its edge.

---
+++ scenes/levels/test.gd
@@ -0,0 +1,31 @@
+extends Node2D
+
+@export var mob_scene: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	print("Test Level ready, adding player")
+	var player: PackedScene = load("res://scenes/player/player.tscn")
+	var player_node: CharacterBody2D = player.instantiate()
+	player_node.position = $PlayerSpawnMarker2D.position
+	
+	add_child(player_node)
+	
+	print("Player Added!")
+	print(get_children(	))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_mob_timer_timeout() -> void:
+	var mob: CharacterBody2D = mob_scene.instantiate()
+	var mob_spawn_location = $MobPath2D/MobSpawnPathFollow2D
+	#get a random location along the path and set it to the mob
+	mob_spawn_location.progress_ratio = randf()
+	mob.position = mob_spawn_location.position
+	
+	
+	add_child(mob)
