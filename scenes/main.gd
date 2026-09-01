80a6880e6b55bc5d7e874e3761764ca0b0e250d1
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-01 12:58:09 -0300

    feat: implement debug mode and update player reference

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,13 +1,26 @@
 extends Node2D
 
+## Time to win in minutes
+const WIN_TIME: float = 5 
+var player: CharacterBody2D
+
+
 var score:int = 0
 
 var game_started: bool = false
 var test_level: PackedScene = preload("res://scenes/levels/test.tscn")
 
+var debug_mode: bool
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	debug_mode = OS.is_debug_build()
 	get_tree().paused = true
+	player = $Player
+	
+func _process(_delta: float) -> void:
+	if debug_mode:
+		_update_debug_ui()
 
 
 func _start_game() -> void:
@@ -48,13 +61,20 @@ func _on_score_timer_timeout() -> void:
 func _set_score(value: int) -> void:
 	score = value
 	$HUD.update_score(value)
+	
+	if score == (60.0 * WIN_TIME):
+		get_tree().paused = true
+		$HUD.show_win()
 
 
 func _on_player_damaged() -> void:
-	var data :Dictionary = $Player.get_player_stats()
+	var data :Dictionary = player.get_player_stats()
 	$HUD.update_health(data.current_hp, data.max_hp)
 
 
 func _on_hud_end_game() -> void:
 	#TODO: save data before quiting?
 	get_tree().quit()
+
+func _update_debug_ui() -> void:
+	$HUD.update_debug(player.get_player_stats(), 60 * WIN_TIME)
