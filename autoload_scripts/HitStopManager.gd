e1e666bbecef482cbbbea001d70592ff79b0a59f
Lucas Luquesi Yasukawa <yasukawa.luquesi.lucas@gmail.com>
2026-09-02 13:55:31 -0300

    fix: add check for time effects before executing hitstop

--- autoload_scripts/HitStopManager.gd
+++ autoload_scripts/HitStopManager.gd
@@ -22,13 +22,18 @@ func _get_duration(duration: Duration) -> float:
 	
 	return time
 
-## Pauses the game for a X duration to create a hit stop effect.
+## Pauses the game for a X duration to create a hit stop effect and then goes back to previous time scale. Only runs if no time effect is happening.
 func hit_stop(duration: Duration):
+	if Engine.time_scale != 1:
+		return
+	
 	var time: float = _get_duration(duration)
-
+	
 	Engine.time_scale = 0
 	await get_tree().create_timer(time, true, false, true).timeout
 	Engine.time_scale = 1
+
+
 func slow_mo(seconds: int):	
 	Engine.time_scale = 0.4
 		
